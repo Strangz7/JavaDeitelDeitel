40522add92821e6a98130d7c036943d00c0256a8
Strangz7 <stephenadebayo79@gmail.com>
2022-05-02 16:24:14 +0100

    assignment

---
+++ src/javaAssignment/Shapes.java
@@ -0,0 +1,15 @@
+package javaAssignment;
+
+public class Shapes {
+    public static void main(String[] args) {
+//        int n = 6;
+        for (int i = 1; i < 5; i++) {
+            System.out.println("");
+            for (int j = 1; j <= i; j++) {
+                System.out.print("1");
+
+            }
+
+        }
+    }
+}
